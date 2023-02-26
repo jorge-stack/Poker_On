50fd9082c61dd2871f84195a6c325f62ec215620
Jorge <saenzdiazj0@gmail.com>
2023-02-25 23:15:50 -0500

    First Commit

---
+++ src/main/java/com/arsw/poker_eci/PokerEciApplication.java
@@ -0,0 +1,13 @@
+package com.arsw.poker_eci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerEciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokerEciApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arsw/poker_eci/PokerEciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsw.poker_eci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerEciApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
